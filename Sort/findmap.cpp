71ddb32d6d789bdc4c03b2c43e79e9ef37325510
13308350476 <zxc723067485811@qq.com>
2023-03-07 18:11:19 +0800

    add bianli

--- Sort/findmap.cpp
+++ Sort/findmap.cpp
@@ -93,20 +93,20 @@ BNODE *b_put(BNODE *node, char k,int v) {
     return node;
 }
 
-int b_get_head(char k) {
+BNODE * b_get_head(char k) {
     return b_get(root,k);
 }
 
-int b_get(BNODE *node, char k) {
+BNODE * b_get(BNODE *node, char k) {
     if(NULL == node) {
-        return -1;
+        return NULL;
     }
     if(k > node->key) {
         return b_get(node->right,k);
     }else if(k < node->key) {
         return b_get(node->left,k);
     }else{
-        return node->value;
+        return node;
     }
 }
 
@@ -148,10 +148,128 @@ int Brank(BNODE *node,char k) {
 
 }
 
+BNODE * Bselect_head(int rank) {
+    return Bselect(root,rank);
+}
+
+BNODE * Bselect(BNODE *node,int rank) {
+    if(rank > size(node)){
+        cout << "error input" << endl;
+        return NULL;
+    }
+    if(1 + size(node->left) == rank){
+        return node;
+    }else if(rank < size(node->left) + 1){
+        return Bselect(node->left,rank);
+    }else{
+        rank = rank- size(node->left) - 1;
+        return Bselect(node->right,rank);
+    }
+}
+
+BNODE * Bmin_head() {
+    return Bmin(root);
+}
+
+BNODE * Bmin(BNODE *node) {
+    if(NULL == node->left){
+        return node;
+    }
+    return(Bmin(node->left));
+}
+
+void deletmin_head() {
+    root = deletmin(root);
+}
+
+BNODE * deletmin(BNODE *node) {
+    if(NULL == node) {
+        cout << "deletmin: node is NULL" << endl;
+        return NULL;
+    }
+    if(NULL == node->left) {
+        BNODE *temp = node->right;
+        delete node;
+        return temp;
+    }
+    node->left = deletmin(node->left);
+    node->N = size(node->left) + size(node->right) + 1;
+    return node;
+}
+
+void Bdelet_head(char k) {
+    root = Bdelet(root,k);
+}
+
+BNODE * Bdelet(BNODE *node, char k) {
+    if(NULL == node) {
+        cout << "error key" << endl;
+        return NULL;
+    }else if(k < node->key){
+        node->left = Bdelet(node->left,k);
+    }else if(k > node->key) {
+        node->right = Bdelet(node->right,k);
+    }else{
+        if(NULL == node->left){
+            BNODE *temp = node->right;
+            delete node;
+            return temp;
+        }
+        if(NULL == node->right) {
+            BNODE *temp = node->left;
+            delete node;
+            return temp;
+        }
+        BNODE *min = Bmin(node->right);
+        node->key = min->key;
+        node->value = min->value;
+        node->right = deletmin(node->right);
+    }
+    node->N = size(node->left) + size(node->right) + 1;//不要忘记加1
+    return node;
+}
+
+void print(BNODE *node) {
+    if(NULL == node) {
+        return;
+    }
+    print(node->left);
+    cout << node->key << " ";
+    print(node->right);
+}
+
+vector<char> find_lo_hi(char lo,char hi) {
+    vector<char> temp;
+    find_rang(root,temp,lo,hi);
+    return temp;
+}
+
+void find_rang(BNODE *node,vector<char> &list,char lo,char hi) {
+    if(NULL == node){
+        return;
+    }
+    if(node->key > lo) {
+        find_rang(node->left,list,lo,hi);
+    }
+    if(lo <= node->key && node->key <= hi) {
+        list.push_back(node->key);
+        cout << node->key << " ";
+    }
+    if(node->key < hi){
+        find_rang(node->right,list,lo,hi);
+    }
+}
+
 int main() {
     // Linked_list_func();
     BST_input_func();
-    // cout << b_get_head('H');
-    cout << rank_head('S') << endl;
+    // cout << b_get_head('H')->value; //要判断是否为空
+    // cout << rank_head('S') << endl;
+    // cout << Bselect_head(6)->key << endl; //要判断是否为空
+    // cout << Bmin_head()->key;
+    // deletmin_head();
+    // Bdelet_head('E');
+    // print(root);
+    find_lo_hi('A','S');
     return 0;
 }
